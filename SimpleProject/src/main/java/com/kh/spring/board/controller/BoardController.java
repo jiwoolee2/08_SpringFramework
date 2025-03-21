45be76a99e40bf97799d284e18252446a7b8e1d1
이지우 <127360928+jiwoolee2@users.noreply.github.com>
2025-03-21 17:47:34 +0900

    0321

--- SimpleProject/src/main/java/com/kh/spring/board/controller/BoardController.java
+++ SimpleProject/src/main/java/com/kh/spring/board/controller/BoardController.java
@@ -1,31 +1,53 @@
 package com.kh.spring.board.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.spring.board.model.dto.BoardDTO;
 import com.kh.spring.board.model.service.BoardService;
+import com.kh.spring.exception.InvalidParameterException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+//@RequestMapping("boards")
 @Slf4j
 @Controller
-//@RequestMapping("boards")
 @RequiredArgsConstructor
 public class BoardController {
 	
 	private final BoardService boardService;
 	
+	// ?Page=1
 	@GetMapping("boards")
-	public String toBoardList() {
+	public String toBoardList(@RequestParam(name="page",defaultValue = "1") int page
+			,Model model) {
+		
+		// 한 페이지에 몇개 보여줄깡? ==5
+		// 버튼 몇개 보여줄깡??     ==5
+		// 총 게시글 개수 == SELECT COUNT(*) FROM TB_SPRING_BOARD WHERE STATUS='Y'
+		if(page<1) {
+			throw new InvalidParameterException("얻;;;");
+		}
+		Map<String, Object> map = boardService.selectBoardList(page);
+		
+		model.addAttribute("map",map);
+		
 		return "board/board_list";
 	}
+	
+	
+	
 	
 	@GetMapping("form.bo")
 	public String goToForm() {
@@ -50,12 +72,29 @@ public class BoardController {
 		// 1. 권한있는 요청인가
 		// 2. 값들이 유효성이 있는 값인가
 		// 3. 전달된 파일이 존재할 경우 => 파일명 수정 서버에 올리고 BoardDTO의 changeName필드에 값을 대입 
+		
 		boardService.insertBoard(board, upfile, session);
-		return null;
+		mv.setViewName("redirect:boards");
+		session.setAttribute("message", "호잇");
+		return mv;
+	}  
+	
+	
+	// 게시판에 올린 글의 상세페이지로 감
+	// 넘어오는 값을 변수로 뽑아서 활용하겠다!!
+	@GetMapping("boards/{id}")
+	public ModelAndView goBoard(@PathVariable(name="id") int boardNo,
+			ModelAndView mv) {
+		
+		log.info("게시글번호 : {}",boardNo);
+		if(boardNo < 1) {
+			throw new InvalidParameterException("비정상적인 접근입니다.");
+		}
+		BoardDTO board = boardService.selectBoard(boardNo);
+		mv.addObject("board",board).setViewName("board/board_detail");
+		
+		return mv;
 	}
-	
-	
-	
 	
 	
 	
--- SimpleProject/src/main/java/com/kh/spring/board/model/service/BoardServiceImpl.java
+++ SimpleProject/src/main/java/com/kh/spring/board/model/service/BoardServiceImpl.java
@@ -1,29 +1,51 @@
 package com.kh.spring.board.model.service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.spring.board.model.dto.BoardDTO;
+import com.kh.spring.board.model.mapper.BoardMapper;
 import com.kh.spring.exception.AuthenticationException;
 import com.kh.spring.exception.InvalidParameterException;
 import com.kh.spring.member.model.dto.MemberDTO;
+import com.kh.spring.reply.model.dto.ReplyDTO;
+import com.kh.spring.util.model.dto.PageInfo;
+import com.kh.spring.util.template.Pagination;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
  
+	private final BoardMapper boardMapper;
+	
 	@Override
-	public void insertBoard(BoardDTO board, MultipartFile file, HttpSession session) {
+	public void insertBoard(BoardDTO board, MultipartFile upfile, HttpSession session) {
 		
 		// 1. 권한 체크
 		MemberDTO loginMember = (MemberDTO)session.getAttribute("loginMember");
 		if(loginMember != null && !loginMember.getMemberId().equals(board.getBoardWriter())) {
 			throw new AuthenticationException("권한 없는 접근입니다.");
 		}
-		
+		log.info("게시글 제목: {}", board.getBoardTitle());
+		log.info("게시글 내용: {}", board.getBoardContent());
+		log.info("작성자: {}", board.getBoardWriter());
 		// 2. 유효성 검사
 		if(board.getBoardTitle() == null || board.getBoardTitle().trim().isEmpty() ||
 		   board.getBoardContent() == null || board.getBoardContent().trim().isEmpty() ||
@@ -33,20 +55,95 @@ public class BoardServiceImpl implements BoardService {
 		
 		// 2_2)
 		
+		// 3) 파일유무체크 // 이름바꾸기 + 저장
+		if(!upfile.getOriginalFilename().isEmpty()) {
+			
+			// 이름바꾸기
+			// KH_현재시간+원본파일확장자
+			
+			StringBuilder sb = new StringBuilder();
+			String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+					log.info("현재시간 : {}", currentTime);
+			int random = (int)(Math.random()*900) + 100;
+			String ext = upfile.getOriginalFilename().substring(upfile.getOriginalFilename().lastIndexOf("."));
+			sb.append("KH_");
+			sb.append(currentTime);
+			sb.append("_");
+			sb.append(random);
+			sb.append(ext);
+			
+			log.info("바뀐 파일명 : {}", sb.toString());
+			
+			// applicationScope(전체에서 사용가능한 저장공간)를 반환 
+			ServletContext application = session.getServletContext();
+			
+			// 저장할 경로
+			String savePath = application.getRealPath("/resources/upload_files/");
+			
+			try {
+				upfile.transferTo(new File(savePath+sb.toString()));
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+			}
+			
+			board.setChangeName("/spring/resources/upload_files/"+sb.toString());
+		}
 		
-		
+		boardMapper.insertBoard(board);
 		
 
 	}
 
 	@Override
-	public List<BoardDTO> selectBoardList(int currentPage) {
-		return null;
+	public Map<String, Object> selectBoardList(int currentPage) {
+		List<BoardDTO> boards = new ArrayList();
+		
+		
+		// 게시글 개수가 없을 경우 DB에 안가고 바로 controller로 예외발생x 로 돌아감
+		int count = boardMapper.selectTotalCount();
+		PageInfo pi = Pagination.getPageInfo(count, currentPage, 5 ,5);
+		
+		if(count != 0) {
+			RowBounds rb = new RowBounds((currentPage -1)*5,5);
+			boards = boardMapper.selectBoardList(rb);
+		}
+		
+		
+		// map에다가 게시글이 있다면 게시글을 리스트에 담고, 페이지에대한 정보를 같이 담아서 리턴
+		Map<String, Object> map = new HashMap();
+		
+		map.put("boards", boards);
+		map.put("pageInfo", pi);
+		
+		return map;
 	}
 
 	@Override
 	public BoardDTO selectBoard(int boardNo) {
-		return null;
+		
+		// 1절
+		//BoardDTO board = boardMapper.selectBoard(boardNo);
+		
+		/* 게시글이 존재하지 않으면 예외 발생*/ 
+		//if(board == null) {
+			//throw new InvalidParameterException("존재하지 않는 게시글입니다.");
+		
+		// 2절
+		//List<ReplyDTO> replyList = boardMapper.selectReply(boardNo);
+		//board.setReplyList(replyList);
+		
+		
+		
+		
+		//3절
+	BoardDTO board = boardMapper.selectBoardAndReply(boardNo);
+	if(board == null) {
+		throw new InvalidParameterException("존재하지않는게시글입니다.");
+		
+	}	
+		
+	
+		return board;
 	}
 
 	@Override
